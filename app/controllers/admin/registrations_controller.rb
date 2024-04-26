faf521a13de33ec92f09bbec0c811e5211953e43
yuushigure27 <yuushigure1227@gmail.com>
2024-04-26 14:28:02 +0000

    add homescontroller

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class Admin::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 
   # GET /resource/sign_up
   # def new
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -3,6 +3,11 @@
 class User::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    # 遷移先のパス
+  end
+  
+  
   # GET /resource/sign_in
   # def new
   #   super
